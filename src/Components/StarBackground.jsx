2b0e45ea37ffc8a553fe2f90b6c5e2e009bf09fd
ManuelPepe129 <manuelpepe1995@gmail.com>
2025-07-20 14:56:14 +0200

    removed meteor effect

--- src/Components/StarBackground.jsx
+++ src/Components/StarBackground.jsx
@@ -4,11 +4,9 @@ import {useEffect, useState} from "react";
 
 export const StarBackground = () => {
     const [stars, setStars] = useState([]);
-    const [meteors, setMeteors] = useState([]);
 
     useEffect(() => {
         generateStars();
-        generateMeteors();
 
         const handleResize = () => {
             generateStars();
@@ -36,24 +34,6 @@ export const StarBackground = () => {
         setStars(newStars);
     };
 
-    const generateMeteors = () => {
-        const numberOfMeteors = 4;
-
-        const newMeteors = [];
-        for (let i = 0; i < numberOfMeteors; i++) {
-            newMeteors.push({
-                id: i,
-                size: Math.random() * 2 + 1,
-                x: Math.random() * 100,
-                y: Math.random() * 20,
-                delay: Math.random() * 15,
-                animationDuration: Math.random() * 3 + 3,
-            });
-        }
-
-        setMeteors(newMeteors);
-    };
-
     return <div className="fixed inset-0 overflow-hidden pointer-events-none z-0">
         {stars.map((star) => (
             <div key={star.id} className="star animate-pulse-subtle" style={{
@@ -63,20 +43,6 @@ export const StarBackground = () => {
                 top: star.y + "%",
                 opacity: star.opacity,
                 animationDuration: star.animationDuration,
-            }}/>
-        ))}
-
-        {meteors.map((meteor) => (
-            <div
-                key={meteor.id}
-                className="meteor animate-meteor"
-                style={{
-                width: meteor.size * 50 + "px",
-                height: meteor.size * 2 + "px",
-                left: meteor.x + "%",
-                top: meteor.y + "%",
-                animationDelay: meteor.delay,
-                animationDuration: meteor.animationDuration + "s",
             }}/>
         ))}
     </div>;
